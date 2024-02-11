46715ed20b0a88b4dc4515f0ef67e06b9db1010a
YuTengjing <ytj2713151713@gmail.com>
2024-02-11 21:31:26 +0800

    允许不写 md description

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,7 +18,7 @@ const blog = defineCollection({
         })
         .or(z.string())
         .optional(),
-      description: z.string(),
+      description: z.string().optional(),
       canonicalURL: z.string().optional(),
     }),
 });
