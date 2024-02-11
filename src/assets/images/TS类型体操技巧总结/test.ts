c02f3bf63ff15efaa2fab0757a84bd4424b9817f
YuTengjing <ytj2713151713@gmail.com>
2024-02-11 11:49:49 +0800

    迁移博客图片

---
+++ src/assets/images/TS类型体操技巧总结/test.ts
@@ -0,0 +1,12 @@
+type Join<
+  Strs extends readonly string[],
+  Result extends string = "",
+> = Strs extends [
+  infer First extends string,
+  ...infer Rest extends readonly string[],
+]
+  ? Join<Rest, `${Result}${First}`>
+  : Result;
+
+type X = Join<["a", "b", "c"]>; // => 'abc'
+//   ^?
