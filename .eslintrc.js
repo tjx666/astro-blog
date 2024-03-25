f3fdf5f148325dd2a07d9f9ad53dff1ddbd2121b
YuTengjing <ytj2713151713@gmail.com>
2024-03-25 23:09:53 +0800

    简化 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
     root: true,
-    plugins: ['simple-import-sort'],
     extends: ['@yutengjing/eslint-config-typescript', 'plugin:astro/recommended'],
     overrides: [
         {
@@ -15,34 +14,4 @@ module.exports = defineConfig({
             rules: {},
         },
     ],
-    rules: {
-        'import/order': 'off',
-
-        // import order
-        'simple-import-sort/imports': [
-            'error',
-            {
-                groups: [
-                    // Side effect imports.
-                    ['^\\u0000'],
-                    // Node.js builtins prefixed with `node:`.
-                    ['^node:'],
-                    // framework
-                    ['^astro', '^react'],
-                    // Packages.
-                    // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-                    ['^@?\\w'],
-                    // source code
-                    ['^@(assets|config|components|content|layouts|pages|styles|utils|)(/.*|$)'],
-                    // Absolute imports and other imports such as Vue-style `@/foo`.
-                    // Anything not matched in another group.
-                    ['^'],
-                    // Relative imports.
-                    // Anything that starts with a dot.
-                    ['^\\.'],
-                ],
-            },
-        ],
-        'simple-import-sort/exports': 'error',
-    },
 });
